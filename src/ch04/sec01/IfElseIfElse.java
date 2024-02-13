b103431fa9607588ce817030318498974e13bc7a
sm9940 <elation4927@gmail.com>
2024-02-13 10:41:34 +0900

    240213

---
+++ src/ch04/sec01/IfElseIfElse.java
@@ -0,0 +1,22 @@
+package ch04.sec01;
+
+public class IfElseIfElse {
+
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >= 90) {
+            System.out.println("점수가 90점 이상입니다.");
+            System.out.println("등급은 A");
+        } else if (score>=80){
+            System.out.println("점수가 80점 이상입니다.");
+            System.out.println("등급은 B");
+        } else if (score>=70) {
+            System.out.println("점수가 70점 이상입니다.");
+            System.out.println("등급은 C");
+        } else {
+            System.out.println("점수가 70점 미만입니다.");
+            System.out.println("등급은 D");
+        }
+    }
+}
